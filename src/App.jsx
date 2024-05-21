a97be258c11d743f65dbe3d76b9535f27dd3ad0e
Fereshte <nikkhah12175@gmail.com>
2024-05-21 17:34:08 +0330

    Revert "User Input & two way binding"
    This reverts commit ba43008d03a380673578f685155bcb0c88cb9b14.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import Player from './components/Player';
+
 
 function App() {
   
 
   return (
-    <main>
-      <div id="game-container">
-      <ol id="players">
-        
-      <Player initialName="Player 1" symbol="X"/>
-      <Player initialName="Player 2" symbol="O"/>
-      </ol>
-
-      </div>
-      LOG
-    </main>
+    
+   <p>Coming soon...everthing gets well</p>
   )
 }
 
